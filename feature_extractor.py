0411e43170aacd064af3c30bc64b222ed54b7157
Joaquín Moreno Guzmán <juaki0315@gmail.com>
2025-06-30 11:42:39 +0200

    mock del extract features para que no pete

--- feature_extractor.py
+++ feature_extractor.py
@@ -9,12 +9,6 @@ modelo = load_model("modelo.h5")
 modelo_features = Model(inputs=modelo.input, outputs=modelo.get_layer("nombre_de_la_capa").output)
 
 def extract_features(img):
-    # Suponiendo img en formato OpenCV (grayscale)
-    img = cv2.resize(img, (224, 224))
-    img = np.expand_dims(img, axis=-1)  # (H, W, 1)
-    img = np.repeat(img, 3, axis=-1)    # (H, W, 3)
-    img = img.astype("float32") / 255.0
-    img = np.expand_dims(img, axis=0)   # (1, H, W, C)
     
-    features = modelo_features.predict(img)
-    return features.squeeze()
+    np.random.seed(42)  # Para que sea reproducible
+    return np.random.rand(512)
