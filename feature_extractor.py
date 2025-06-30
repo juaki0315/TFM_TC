0e410d097860d484f14a61e6e43bf8ac1b1e3b7b
Joaquín Moreno Guzmán <juaki0315@gmail.com>
2025-06-30 11:38:40 +0200

    cambio

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,6 +1,20 @@
 import numpy as np
+from tensorflow.keras.models import load_model, Model
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.image import resize
+import cv2
+
+# Cargar modelo completo y truncarlo
+modelo = load_model("modelo.h5")
+modelo_features = Model(inputs=modelo.input, outputs=modelo.get_layer("nombre_de_la_capa").output)
 
 def extract_features(img):
-    # Simula las 512 features de una CNN real
-    np.random.seed(42)  # Para que sea reproducible
-    return np.random.rand(512)
+    # Suponiendo img en formato OpenCV (grayscale)
+    img = cv2.resize(img, (224, 224))
+    img = np.expand_dims(img, axis=-1)  # (H, W, 1)
+    img = np.repeat(img, 3, axis=-1)    # (H, W, 3)
+    img = img.astype("float32") / 255.0
+    img = np.expand_dims(img, axis=0)   # (1, H, W, C)
+    
+    features = modelo_features.predict(img)
+    return features.squeeze()
